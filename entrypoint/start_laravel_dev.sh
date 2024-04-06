9ea5e4267f7cb4335c59081a3e912e034b5cd7f1
rreeggeenntt4 <rreeggeenntt4@yandex.ru>
2024-04-06 16:13:55 +0300

    update

--- entrypoint/start_laravel_dev.sh
+++ entrypoint/start_laravel_dev.sh
@@ -2,8 +2,13 @@
 cd /app/laravel
 if [[ -d "vendor" ]]; then
   echo "vendor directory exists";
+  #composer update;
+  #composer dump-autoload;
+
 else
   composer install
+  composer update;
+  composer dump-autoload;
   php artisan key:generate
   php artisan config:cache
 fi;
--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\FlightController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get('/aircraft_airports', [FlightController::class, 'getAircraftAirports']);
